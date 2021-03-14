80eebbad3c84b468887aca9b91bc42520ae42278
Vinícius Correia <softmakerinfos@gmail.com>
2021-03-14 19:31:09 -0300

    style

---
+++ src/screens/Welcome/styles.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  textFooter: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: "#808080"
+  },
+  textBody: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  button: {
+    width: '48%',
+    backgroundColor: '#1D2362',
+    borderRadius: 10,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default styles;
