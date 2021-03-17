ac2172781ccab9b3f86cedc630f6a2de3d26fbda
Vinícius Correia <softmakerinfos@gmail.com>
2021-03-16 23:36:48 -0300

    tela settings

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,35 +1,51 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Button } from 'react-native-elements';
 
 export default function Settings() {
-  
+
   const navigation = useNavigation();
-  
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}> 
-      <Text>Settings</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', padding: 40 }}>
+      <Text style={{ padding: 10, fontSize: 25, fontWeight: 'bold', }}>Settings</Text>
 
-      <View>
-        <TouchableOpacity
-          onPress={()=> navigation.navigate('AccountSettings')}
-       >
-          <Text>Conta</Text>
-        </TouchableOpacity>
-      </View> 
 
-      <View > 
-        <TouchableOpacity
-          onPress={()=> {
+      <View style={{ padding: 20, width: 200, margin: 10 }}>
+        <Button
+          icon={
+            <Icon
+              name="user-circle"
+              size={15}
+              color="white"
+            />
+          }
+          title=" Conta"
+          onPress={() => navigation.navigate('AccountSettings')}
+        />
+      </View>
+
+      <View style={{ padding: 20, width: 200 }}>
+
+        <Button
+          icon={
+            <Icon
+              name="arrow-right"
+              size={15}
+              color="white"
+            />
+          }
+          title=" Sair"
+          onPress={() => {
             AsyncStorage.removeItem('@loggedUser')
             navigation.navigate('Login')
           }}
-        >
-          <Text>Sair</Text>
-        </TouchableOpacity>
-      </View> 
+        />
+      </View>
     </View>
   );
 }
