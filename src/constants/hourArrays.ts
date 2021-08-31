ccf2dc4c81a6ce9f30edfac11b2fc47a886f5ac5
Vinícius Correia <softmakerinfos@gmail.com>
2021-08-31 17:27:33 -0300

    starting reserves functionalities

---
+++ src/constants/hourArrays.ts
@@ -0,0 +1,41 @@
+export const startHourArray = 
+[
+    { hour: 7, minute: 30 },
+    { hour: 8, minute: 20 },
+    { hour: 9, minute: 20 },
+    { hour: 10, minute: 10 },
+    { hour: 11, minute: 50 },
+
+    { hour: 13, minute: '00' },
+    { hour: 13, minute: 50 },
+    { hour: 14, minute: 50 },
+    { hour: 15, minute: 40 },
+    { hour: 16, minute: 40 },
+    { hour: 17, minute: 30 },
+
+    { hour: 19, minute: '00' },
+    { hour: 19, minute: 50 },
+    { hour: 20, minute: 50 },
+    { hour: 21, minute: 40 },
+];
+
+export const endHourArray = 
+[
+    { hour: 8, minute: 20 },
+    { hour: 9, minute: 10 },
+    { hour: 10, minute: 10 },
+    { hour: 11, minute: 0 },
+    { hour: 11, minute: 50 },
+
+    { hour: 13, minute: 50 },
+    { hour: 14, minute: 40 },
+    { hour: 15, minute: 40 },
+    { hour: 16, minute: 30 },
+    { hour: 17, minute: 30 },
+    { hour: 18, minute: 20 },
+
+    { hour: 19, minute: 50 },
+    { hour: 20, minute: 40 },
+    { hour: 21, minute: 40 },
+    { hour: 22, minute: 30 },
+];
--- src/screens/NewReserve/NewEquipmentReserve/index.tsx
+++ src/screens/NewReserve/NewEquipmentReserve/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Text, View } from 'react-native';
+import { Alert, Text, View, LogBox, SafeAreaView, ScrollView, TextInput, StyleSheet } from 'react-native';
 // import { useAuth } from '../../hooks/auth';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native-elements';
@@ -7,8 +7,14 @@ import { Icon } from 'react-native-elements/dist/icons/Icon';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
 import { FlatList, TouchableHighlight } from 'react-native-gesture-handler';
-
+import { Picker } from '@react-native-community/picker'
 import DateTimePicker from '@react-native-community/datetimepicker';
+
+import EquipmentReserveTouchable from '../../../components/EquipmentReserveTouchable';
+
+import { startHourArray } from '../../../constants/hourArrays';
+import { split } from 'lodash';
+
 
 
 interface Equipment {
@@ -17,20 +23,71 @@ interface Equipment {
   description: string
 }
 
+interface IarrayHour {
+  hour: number,
+  minute: number
+}
+
+
 export default function NewEquipmentReserve() {
   const navigation = useNavigation();
 
 
   const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState('date');
+  const [hour, setHour] = useState({});
+  const [equipments, setEquipments] = useState([{}]);
   const [show, setShow] = useState(false);
 
+  const fetchEquipments = async (data) => {
+
+    console.log(data)
+    try {
+      const token = await AsyncStorage.getItem('@EReserva:token');
+
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+        params: {
+          date: data.date,
+          hour: data.hour,
+          minute: data.minute
+        }
+      };
+      const response = await api.get('/reserves/equipments/available', config);
+      setEquipments(response.data);
+    } catch (err) {
+      Alert.alert('Falha ao carregar equipamentos!')
+      // console.log(err)
+    }
+  }
+
   const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    console.log(currentDate),
-      setShow(Platform.OS === 'ios');
-    if (currentDate != undefined) {
-      setDate(currentDate);
+    // setHour({});
+
+    setDate(selectedDate.getTime());
+    // 
+
+    // const data = currentDate.toString();
+
+    // const arrayDate = data.split(" ");
+
+    // console.log(arrayDate);
+
+    // const day = arrayDate[2];
+    // const year = arrayDate[3];
+
+    // console.log(day);
+    // console.log(year);
+
+
+    // data.split("-");
+    // console.log(data[]);
+
+    // console.log(currentDate),
+
+    setShow(Platform.OS === 'ios');
+    if (selectedDate != undefined) {
+      setDate(selectedDate.getTime());
     }
   };
 
@@ -46,6 +103,21 @@ export default function NewEquipmentReserve() {
   const showTimepicker = () => {
     showMode('time');
   };
+  const loadEquipment = (time: IarrayHour) => {
+
+    const hourMinute = time.split(':');
+
+    setHour(time);
+
+    const data = {
+      date,
+      hour: hourMinute[0], //posição 0 encontra-se a hora
+      minute: hourMinute[1] //posição 1 encontra-se os minutos 
+    }
+
+    fetchEquipments(data)
+
+  };
 
   useEffect(() => {
   }, []);
@@ -56,6 +128,10 @@ export default function NewEquipmentReserve() {
         <View>
           <Button onPress={showDatepicker} title="Selecione a data" />
         </View>
+        {/* 
+        <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View> */}
 
         {show && (
           <DateTimePicker
@@ -67,7 +143,51 @@ export default function NewEquipmentReserve() {
             onChange={onChange}
           />
         )}
+
+        <Text style={styles.textInput}>Horário</Text>
+        <Picker
+          selectedValue={hour}
+          onValueChange={(item, index) => { loadEquipment(item) }}>
+          <Picker.Item label="Selecione um Horário" value={''} />
+          {startHourArray.map((hour: IarrayHour) => {
+            return <Picker.Item key={hour.hour} label={`${hour.hour}:${hour.minute}`} value={`${hour.hour}:${hour.minute}`} />
+          })}
+        </Picker>
+
+
+        {equipments ?
+
+          <View style={{ padding: 20, margin: 10 }}>
+            <Text>Equipamentos:</Text>
+            <FlatList
+              data={equipments}
+              renderItem={({ item, index, separators }) => (
+                <View style={{ padding: 20, margin: 10 }}>
+                  <EquipmentReserveTouchable reserve={item} onPress={() => { }} />
+                </View>
+              )}
+            />
+          </View>
+          :
+          null}
+
+
       </View>
     </View >
   );
 }
+
+const styles = StyleSheet.create({
+  formContainer: {
+    padding: 20,
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    marginTop: 15,
+    fontWeight: 'bold',
+    fontSize: 17
+  },
+  divider: {
+    padding: 20
+  },
+});
